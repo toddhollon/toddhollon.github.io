415001d42a79f0d8d03073983ce2038801ed91d7
toddhollon <toddhollon@gmail.com>
2026-03-22 20:22:18 +0000

    Deploying to gh-pages from @ toddhollon/toddhollon.github.io@652e01eeed49acb0287250f495d79df2a08b1c83 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "toddhollon/toddhollon.io",
+    "data-repo": "toddhollon/toddhollon.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
